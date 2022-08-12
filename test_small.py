2dee8e6687d2e6b68d1648009d2f1dec70ff278b
Svltester <111002904+Svltester@users.noreply.github.com>
2022-08-13 00:13:00 +0700

    Update test_small.py

--- test_small.py
+++ test_small.py
@@ -3,7 +3,7 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 def test_am():
 
     def run(playwright: Playwright) -> None:
-        browser = playwright.chromium.launch(headless=True)
+        browser = playwright.chromium.launch(headless=True, chromium_sandbox=True)
         context = browser.new_context()
 
         # Open new page
