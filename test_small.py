cafe26eca268dab0d41622c0afba33b7059ab787
Svltester <111002904+Svltester@users.noreply.github.com>
2022-08-12 23:52:45 +0700

    Update test_small.py

--- test_small.py
+++ test_small.py
@@ -9,18 +9,18 @@ def test_am():
         # Open new page
         page = context.new_page()
 
-        page.goto("http://ia-test.egisz.rosminzdrav.ru/webadmin")
+        page.goto("https://ia-test.egisz.rosminzdrav.ru/webadmin")
     
         # Click text=Вход >> nth=0
         page.locator("text=Вход").first.click()
 
         # Click text=Вход через ЕСИА (тестовая)
         page.locator("text=Вход через ЕСИА (тестовая)").click()
-        page.wait_for_url("http://esia-portal1.test.gosuslugi.ru/login/")
+        page.wait_for_url("https://esia-portal1.test.gosuslugi.ru/login/")
 
         # Click text=Единая система<br/> идентификации и аутентификации
         page.locator("text=Единая система<br/> идентификации и аутентификации").click()
-        page.wait_for_url("http://www.gosuslugi.ru/")
+        page.wait_for_url("https://www.gosuslugi.ru/")
 
         # Click text=Главная >> nth=1
         assert page.locator("text=Главная >> nth=1").is_visible()
