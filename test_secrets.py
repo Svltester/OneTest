8e34ae81d5c3a201feb2e1b91681be98e6de0e57
Svltester <111002904+Svltester@users.noreply.github.com>
2022-08-14 16:49:07 +0700

    Update test_secrets.py

--- test_secrets.py
+++ test_secrets.py
@@ -2,9 +2,11 @@ import os
 
 
 class my_secrets:
-    login = os.getenv('A_USER_LOGIN')
 
+    login1 = os.getenv("A_USER_LOGIN")
+    login2 = os.getenv("A_USER_P")
 
 def test_env():
-    # assert len(my_secrets.login) != 0
-    assert len(my_secrets.login) == len("qwertyuiop")
+    assert len(my_secrets.login1) != 0
+    assert len(my_secrets.login1) == len("qwertyuiop")
+    assert len(my_secrets.login2) == 9
